606c6fa718e533c13ff8628d0e8c53a0857d7a79
laizask <laiza123barbosa@gmail.com>
2024-04-26 15:10:15 -0300

    primeiros codigos em java

---
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/edu/laiza/segundasemana/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+public static void main (String [] args ) {
+
+    String primeiroNome = "Laiza";
+    String segundoNome = "Barbosa";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Redultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
