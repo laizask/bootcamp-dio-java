e13bed6a21e9232ab19be8e6ec90a9475255cf12
laizask <laiza123barbosa@gmail.com>
2024-04-26 17:38:21 -0300

    sistema smart

---
+++ meu-primeiro-projeto-java/java-anatomia-classes/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[]args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 || condicao2) {
+            System.out.println("uma das condicoes são verdadeiras");
+        }
+        System.out.println("fim");
+
+    }
+    
+}
