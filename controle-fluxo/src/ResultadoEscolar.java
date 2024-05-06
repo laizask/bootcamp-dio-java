594c5116c95e411805e47ae134a8bf54079e8b83
laizask <laiza123barbosa@gmail.com>
2024-05-06 14:05:25 -0300

    exemplos

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,9 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
-
-        if(nota >= 7)
-        System.out.println("Aprovado");
-
-        else
-        System.out.println("Reprovado");
+        int nota = 7;
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+;
     }
     
 }
--- sistemas/java-terminal/src/AboutMe.java
+++ sistemas/java-terminal/src/AboutMe.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        //criando o objeto scanner
+        // Criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
-        System.out.println("Digite seu nome");
+        System.out.println("Digite seu nome:");
         String nome = scanner.next();
         
-        System.out.println("Digite seu sobrenome");
+        System.out.println("Digite seu sobrenome:");
         String sobrenome = scanner.next();
 
-        System.out.println("Digite sua idade");
+        System.out.println("Digite sua idade:");
         int idade = scanner.nextInt();
         
-        System.out.println("Digite sua altura");
+        System.out.println("Digite sua altura:");
         double altura = scanner.nextDouble();
 
-        
-        //imprimindo os dados obtidos pelo usuario
-        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close();
+        // Imprimindo os dados obtidos pelo usuário
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos.");
+        System.out.println("Minha altura é " + altura + " cm.");
         
         
     }
