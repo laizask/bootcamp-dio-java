b18d4f92836e254d5ffcde62ebbc24da259437e2
laizask <laiza123barbosa@gmail.com>
2024-05-06 19:01:59 -0300

    exemplos

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = { "LAIZA", "DIEGO", "ANDRE", "JOAO"};
+
+
+        for(int x=0; x < alunos.length; x++) {
+            System.out.println("O aluno no índice x=" + x + "é" + alunos [ x ]);
+        }
+    }
+    
+}
---
+++ controle-fluxo/src/ExemploForEach.java
@@ -0,0 +1,8 @@
+public class ExemploForEach {
+    public static void main(String[] args) {
+        String alunos [] = { "LAIZA", "DIEGO", "ANDRE", "JOAO"};
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
